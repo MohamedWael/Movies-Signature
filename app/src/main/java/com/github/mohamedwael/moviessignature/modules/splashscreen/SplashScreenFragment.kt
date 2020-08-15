83938b85fb6d40e55c666b04e5684394f333781e
Mohamed Wael <imohamedwael@outlook.com>
2020-08-16 01:34:45 +0200

    implementing splash screen

--- app/src/main/java/com/github/mohamedwael/moviessignature/modules/splashscreen/SplashScreenFragment.kt
+++ app/src/main/java/com/github/mohamedwael/moviessignature/modules/splashscreen/SplashScreenFragment.kt
@@ -1,12 +1,13 @@
 package com.github.mohamedwael.moviessignature.modules.splashscreen
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.github.mohamedwael.moviessignature.R
 import com.github.mohamedwael.moviessignature.databinding.SplashScreenFragmentBinding
 
@@ -28,6 +29,16 @@ class SplashScreenFragment : Fragment() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.home.observe(viewLifecycleOwner, Observer {
+            if (it.getContentIfNotHandled() == true){
+                Navigation.findNavController(view).navigate(R.id.action_splashScreenFragment_to_moviesFragment)
+            }
+        })
     }
 
 
--- app/src/main/java/com/github/mohamedwael/moviessignature/modules/splashscreen/SplashScreenViewModel.kt
+++ app/src/main/java/com/github/mohamedwael/moviessignature/modules/splashscreen/SplashScreenViewModel.kt
@@ -1,7 +1,29 @@
 package com.github.mohamedwael.moviessignature.modules.splashscreen
 
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.blogspot.mowael.utilslibrary.utils.SingleLiveDataEvent
+
+const val SPLASH_SCREEN_DURATION = 3000L
 
 class SplashScreenViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val handler = Handler(Looper.getMainLooper())
+    private val _home = MutableLiveData<SingleLiveDataEvent<Boolean>>()
+    val home: LiveData<SingleLiveDataEvent<Boolean>> = _home
+
+    init {
+        handler.postDelayed(::fireNav, SPLASH_SCREEN_DURATION)
+    }
+
+    private fun fireNav() {
+        _home.value = SingleLiveDataEvent(true)
+    }
+
+    override fun onCleared() {
+        handler.removeCallbacks(::fireNav)
+        super.onCleared()
+    }
 }
